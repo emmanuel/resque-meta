a94dce5f0e672a890ebb51beb587f9f15ae861a9
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-03-22 12:36:45 -0700

    Rename JobIdentity#job_id to #job_identity, and changed the signature.
    Now it takes a single argument: an array of args, instead of a list of args, as previously.

--- lib/resque/plugins/job_identity.rb
+++ lib/resque/plugins/job_identity.rb
@@ -13,19 +13,22 @@ module Resque
       # The returned job_id can be used to refer to the job in the future.
       # prepends enqueued job_id to args
       def enqueue(*args)
-        job_id = job_id(args)
+        job_id = job_identity(args)
+
         before_enqueue_hooks = Resque::Plugin.before_enqueue_hooks(self)
         before_enqueue_hooks.each do |hook|
           send(hook, job_id, *args)
         end
 
         Resque.enqueue(self, job_id, *args)
+
         job_id
       end
 
       # Override in your job to control the job id. It is passed the same
       # arguments as `perform`, that is, your job's payload.
-      def job_id(*args)
+      # NOTE: expects a single args array, not a list of args!
+      def job_identity(args)
         Digest::SHA1.hexdigest([ Time.now.to_f, rand, self, args ].join)
       end
 
